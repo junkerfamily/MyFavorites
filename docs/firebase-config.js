daa8b7bdc780ddcf30e567e9b575f38096c449a0
Dan Junker <junkerfamily@hotmail.com>
2025-09-24 13:00:43 -0500

    chore: add docs folder for GitHub Pages deployment

---
+++ docs/firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+// Replace the values below with your Firebase project credentials
+const firebaseConfig = {
+  apiKey: "AIzaSyD4m5AY7QJKAm3MPdbvKBJmPHKCcE8L0WM",
+  authDomain: "my-favorites-bfd1e.firebaseapp.com",
+  projectId: "my-favorites-bfd1e",
+  storageBucket: "my-favorites-bfd1e.firebasestorage.app",
+  messagingSenderId: "700698974659",
+  appId: "1:700698974659:web:672bcfc6b5497f12ef8576"
+};
